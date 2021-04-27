3a8a871f694a037c6bf40f97261641e64e28a2ba
Xurxo Martínez Ferreira <>
2021-04-27 08:57:06 +0000

    master

--- comandos/básicos/ping.js
+++ comandos/básicos/ping.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'ping',
   description: 'Ping!',
   cooldown: 5,
-  execute(message) {
+  execute(client, message) {
     let ping = Math.floor(message.client.ws.ping);
     message.delete();
     message.channel.send('El ping es de `' + ping + ' ms.`').then(msg => {
